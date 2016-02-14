3811594d1bdc6844d810b3f6cd0428f15568f2c2
Joey Buczek <joe.buczek@gmail.com>
2016-02-14 10:33:13 -0500

    "updated json stringify"

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.get(/^\/new\/http?s?\:\/\/[a-z0-9]+[.]+[a-z0-9]+/i, function (req, res) {
     // mongo logic - pass in url, receive response
     mongoFn.mongoNewUrl(reqUrl, function (responseObj) {
         // res.json(responseObj);
-        res.send(JSON.stringify(responseObj, null, 4));
+        res.send(JSON.stringify(responseObj, null, '\t'));
     });    
 });
 
